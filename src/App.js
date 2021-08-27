e4e248d411c5724ed834894cb54e16dc65201ab4
Luis Felipe <l_felipelizeu@hotmail.com>
2021-08-27 20:46:43 -0300

    Now the option is selected when is clicked. Created an exclusive file Confirm.js. Destructuring made at Option component.

--- src/App.js
+++ src/App.js
@@ -8,35 +8,6 @@ export default function App () {
             <Header />
             <Main />
             <Footer />
-            <Confirm />
         </>
-    );
-}
-
-function Confirm () {
-    return (
-        <section class="confirm hidden">
-            <div class="box">
-                <h2>Confirme seu pedido</h2>
-                <div class="row food">
-                    <p class="name">Prato</p>
-                    <p class="price">0</p>
-                </div>
-                <div class="row drink">
-                    <p class="name">Bebida</p>
-                    <p class="price">0</p>
-                </div>
-                <div class="row dessert">
-                    <p class="name">Sobremesa</p>
-                    <p class="price">0</p>
-                </div>
-                <div class="row total">
-                    <p>TOTAL</p>
-                    <p class="price">0</p>
-                </div>
-                <button class="send-order">Tudo certo, pode pedir!</button>
-                <button class="cancel">Cancelar</button>
-            </div>
-        </section>
     );
 }
