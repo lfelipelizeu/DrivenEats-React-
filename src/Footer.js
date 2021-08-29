dd4eb0d74274f9aa1f0eacd75b221eeba85ce9ef
Luis Felipe <l_felipelizeu@hotmail.com>
2021-08-28 23:11:40 -0300

    Created the function to send the order by WhatApp

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,22 @@
 export default function Footer ({ orderList }) {
     const [ foods, drinks, desserts ] = orderList;
 
+    function sendOrder () {
+        const foodsMessage = foods.map((food) => `- Prato: ${food.name} (${food.quantity}x)\n`);
+        const drinksMessage = drinks.map((drink) => `- Bebida: ${drink.name} (${drink.quantity}x)\n`);
+        const dessertsMessage = desserts.map((desserts) => `- Sobremesa: ${desserts.name} (${desserts.quantity}x)\n`);
+        const number = "5511999999999"
+        let total = 0;
+        orderList.forEach((order) => order.forEach((item) => {total += item.price*item.quantity}));
+        const message = `Olá, gostaria de fazer o pedido:\n${foodsMessage}${drinksMessage}${dessertsMessage}Total: R$ ${total.toFixed(2)}`;
+
+        window.open(`https://wa.me/${number}?text=${encodeURIComponent(message.replace(/,/gi, ""))}`);
+    }
+
     if ( foods.length > 0 && drinks.length > 0 && desserts.length > 0 ) {
         return (
             <footer>
-                <button class="enabled">Fechar pedido</button>
+                <button class="enabled" onClick={sendOrder}>Fechar pedido</button>
             </footer>
         );
     } else {
