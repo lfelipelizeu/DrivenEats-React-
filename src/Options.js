9d3f90a3e8a1d7bb0d2100c7c6078012d5ccfb70
Luis Felipe <l_felipelizeu@hotmail.com>
2021-08-28 21:33:49 -0300

    The arrays of the categories are propely update when the option quantity changes

--- src/Options.js
+++ src/Options.js
@@ -8,35 +8,57 @@ export default function Options ({ optionsList, typeList, setTypeList }) {
     );
 }
 
-function Option ({ info, typeList, setTypeList, orderList }) {
+function Option ({ info, typeList, setTypeList }) {
     const { img, name, description, price } = info;
 
     const [classes, setClasses] = useState("option");
     const [quantity, setQuantity] = useState(1);
 
-    function select () {
-        if (classes !== "option selected") {
-            setClasses("option selected");
-
+    function updateTypeList (operation) {
+        if (operation === "add") {
             const newTypeList = [...typeList];
             newTypeList.push({name, price, quantity: 1});
             setTypeList(newTypeList);
+        } else if (operation === "remove") {
+            const newTypeList = typeList.filter((item) => item.name !== name);
+            setTypeList(newTypeList);
+        } else {
+            const newTypeList = [...typeList];
+            newTypeList.forEach((item) => {
+                if (item.name === name) {
+                    if (operation === "increase") {
+                        item.quantity++;
+                    } else {
+                        item.quantity--;
+                    }
+                }
+            });
+            setTypeList(newTypeList);
+        }
+    }
+
+    function select () {
+        if (classes !== "option selected") {
+            setClasses("option selected");
+            updateTypeList("add");
         }
     }
 
     function reduceQuantity (event) {
         event.stopPropagation();
-
         if (quantity-1 === 0) {
             setClasses("option");
+            updateTypeList("remove");
         } else {
             setQuantity(quantity - 1);
+            updateTypeList("reduce");
         }
     }
 
     function increaseQuantity(event) {
         event.stopPropagation();
         setQuantity(quantity + 1);
+        updateTypeList("increase");
     }
 
     return (
